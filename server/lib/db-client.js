0a8fc40d0cde6ade2eecff0f00ba84897d30238d
Ivan <i.poblete915@gmail.com>
2018-12-10 15:33:23 -0800

    setup db-client

---
+++ server/lib/db-client.js
@@ -0,0 +1,14 @@
+const pg = require('pg');
+const DATABASE_URL = 'postgres://localhost:5432/mygoals';
+const Client = pg.Client;
+const client = new Client(DATABASE_URL);
+
+client.connect()
+  .then(() =>console.log('connected to db', DATABASE_URL))
+  .catch(err => console.error('connection error', err));
+
+client.on('error', err => {
+  console.error('\n**** DATABASE ERROR ****\n\n', err);
+});
+
+module.exports = client;
