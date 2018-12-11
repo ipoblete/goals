06c27f7dc3dbbb940bb537bb83bc4ac73f5989d1
Ivan <i.poblete915@gmail.com>
2018-12-10 18:22:04 -0800

    got signup to display on postman

--- server/lib/app.js
+++ server/lib/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const auth = require('./routes/auth');
 
 app.use(morgan('dev'));
 app.use(express.json());
+app.use('/api/auth', auth);
 
 module.exports = app;
---
+++ server/lib/routes/auth.js
@@ -0,0 +1,52 @@
+const router = require('express').Router();
+const client = require('../db-client');
+
+router
+  .post('/signup', (req, res) => {
+    
+    const body = req.body;
+    const username = body.username;
+    const firstName = body.firstName;
+    const lastName = body.lastName;
+    const email = body.email;
+    const password = body.password;
+
+    if(!username || !firstName || !lastName || !email || !password) {
+      res.status(400).json({ error: 'all fields required' });
+      return;
+    }
+    client.query(`
+      SELECT id
+      FROM profile
+      WHERE username = $1;
+    `,
+    [username])
+      .then(result => {
+        if(result.rows.length > 0) {
+          res.status(400).json({ error: 'username already exists'});
+          return;
+        }
+        console.log('creating new user profile...');
+
+        client.query(`
+          INSERT into profile (
+            username, 
+            first_name,
+            last_name,
+            email,
+            password
+           )
+           VALUES ($1, $2, $3, $4, $5)
+           RETURNING id, username;
+        `,
+        [username, firstName, lastName, email, password]
+        )
+          .then(result => {
+            res.json(result.rows[0]);
+          });
+      });
+
+
+  });
+
+module.exports = router;
