d4c41029285f7e6a3c2adbaa400b901abcc59b9c
Ivan <i.poblete915@gmail.com>
2018-12-11 13:45:39 -0800

    able to display sign in error on postman

--- server/lib/routes/auth.js
+++ server/lib/routes/auth.js
@@ -45,8 +45,34 @@ router
             res.json(result.rows[0]);
           });
       });
+  })
 
+  .post('/signin', (req, res) => {
+    const body = req.body;
+    const username = body.username;
+    const password = body.password;
 
+    if(!username || !password) {
+      res.status(400).json({ error: 'username and password required' });
+      return;
+    }
+    client.query(`
+      SELECT id, username, password
+      FROM profile
+      WHERE username = $1;
+    `,
+    [username]
+    )
+      .then(result => {
+        if(result.rows.length === 0 || result.rows[0].password !== password) {
+          res.status(400).json({ error: 'username or password incorrect' });
+          return;
+        }
+        res.json({
+          id: result.rows[0].id,
+          username: result.rows[0].username
+        });
+      });
   });
 
 module.exports = router;
