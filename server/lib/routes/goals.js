b89c530a86ed085995fe81fed4c63e9542aa85ce
Ivan <i.poblete915@gmail.com>
2018-12-13 15:28:03 -0800

    finished setting up goals on server side

--- server/lib/routes/goals.js
+++ server/lib/routes/goals.js
@@ -1,12 +1,53 @@
-// const express = require('express');
-// const client = require('../db-client');
-// const Router = express.Router;
-// const router = Router();
+const express = require('express');
+const client = require('../db-client');
+const Router = express.Router;
+const router = Router();
 
-// router
-//   .get('/', (req, res) => {
-//     client.query(`
-//       SELECT id,
-//     `,
-//     )
-//   })
+router
+  .get('/', (req, res) => {
+    client.query(`
+      SELECT 
+      id, 
+      title, 
+      start_date as "startDate"
+      FROM goal
+      WHERE profile_id = $1;
+    `,
+    [req.userId])
+      .then(result => {
+        res.json(result.rows);
+      });
+  })
+
+  .post('/', (req, res) => {
+    const body = req.body;
+
+    client.query(`
+      INSERT INTO pet (title, start_date, profile_id)
+      VALUES($1, $2, $3)
+      RETURNING *;
+    `,
+    [body.title, body.startDate, req.userId])
+      .then(result => {
+        res.json(result.rows[0]);
+      });
+  })
+
+  .put('/:id/completed', (req, res) => {
+    const completed = req.body.completed;
+
+    client.query(`
+      UPDATE goal
+      SET completed = $1
+      WHERE id = $2
+      AND profile_id = $3
+      RETURNING *;
+    `,
+    [completed, req.params.id, req.userId]
+    )
+      .then(result => {
+        res.json(result.rows[0]);
+      });
+  });
+
+module.exports = router;
