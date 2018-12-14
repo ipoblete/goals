edb7a3d8b04851c2e791f4eafd69d4c8362adc85
Ivan <i.poblete915@gmail.com>
2018-12-13 22:59:18 -0800

    produced web token for sign in on postman

--- server/lib/routes/auth.js
+++ server/lib/routes/auth.js
@@ -76,8 +76,9 @@ router
           return;
         }
         res.json({
-          id: result.rows[0].id,
-          username: result.rows[0].username
+          id: profile.id,
+          username: profile.username,
+          token: jwt.sign({ id: profile.id }, APP_SECRET)
         });
       });
   });
